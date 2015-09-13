00120ae9bbd494912291daa81e6a0ebea37d623f
porfirovskiy <porfirovskiy@gmail.com>
2015-09-13 23:39:44 +0400

    Create tags

---
+++ app/controllers/Tag.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace SiteMicroEngine\App\Controllers;
+
+/**
+ * Description of Tag
+ *
+ * @author porfirovskiy
+ */
+class Tag extends Controller {
+    public function actionIndex() {
+        $model = $this->getModel();
+        $tags = $model->getAllTages();
+        $this->render('index', $tages);
+    }
+}
---
+++ app/views/Image/view.php
@@ -0,0 +1,8 @@
+<?php
+echo $data['title'];
+?>
+<br>
+<img alt="Mergedisputed.svg" src="/<?php echo $data['link']; ?>" 
+     height="200" width="300">
+
+/*портал изображений!!! свой - сто пудов взлетит!!!!*/
